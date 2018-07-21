df95b392dc2867a50e85ff7c6f096d6490b45cc5
crismozee <crismoz1986@gmail.com>
2018-07-21 12:51:13 -0400

    It worked

--- FirstCoding/app.js
+++
@@ -1,9 +0,0 @@
-// Hide the Quote text
-$('.Quote span').hide();
-// When the Quote button is pressed
-$('.Quote button').click(function(){
-   //Show the Quote text
-   $('.Quote span').show();
-    //Hide the "Quote" button 
-    $('.Quote button').hide();
-});
